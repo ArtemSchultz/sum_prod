227ad4d2bd5c4dfef2b3f2f8cc1dab41e74ba043
st144254 <st144254@sudent.spbu.ru>
2025-10-21 14:54:07 +0300

    fix: test.sh

--- test.sh
+++ test.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-./sum_prod < ./tests/001.dat > output.txt
+./sumprod < ./tests/001.dat > output.txt
 cmp output.txt ./tests/001.ans && echo "test 1 - OK" || exit 1
 
-./sum_prod < ./tests/002.dat > output.txt
+./sumprod < ./tests/002.dat > output.txt
 cmp output.txt ./tests/002.ans && echo "test 2 - OK" || exit 1
 
-./sum_prod < ./tests/003.dat > output.txt
+./sumprod < ./tests/003.dat > output.txt
 cmp output.txt ./tests/003.ans && echo "test 3 - OK" || exit 1
 
-./sum_prod < ./tests/004.dat > output.txt
+./sumprod < ./tests/004.dat > output.txt
 cmp output.txt ./tests/004.ans && echo "test 4 - OK" || exit 1
 
-./sum_prod 123  > output.txt
+./sumprod 123  > output.txt
 cmp output.txt ./tests/001.ans && echo "test 5 - OK" || exit 1
 
-./sum_prod 99 > output.txt
+./sumprod 99 > output.txt
 cmp output.txt ./tests/002.ans && echo "test 6 - OK" || exit 1
 
-./sum_prod 0 > output.txt
+./sumprod 0 > output.txt
 cmp output.txt ./tests/003.ans && echo "test 7 - OK" || exit 1
 
-./sum_prod 101  > output.txt
+./sumprod 101  > output.txt
 cmp output.txt ./tests/004.ans && echo "test 8 - OK" || exit 1
